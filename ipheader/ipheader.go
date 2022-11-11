200672bb1fe34d2b297fc5ec7b388f69ddc2e591
basi <basi-a@outlook.com>
2022-11-11 11:49:11 +0800

    IP header

---
+++ ipheader/ipheader.go
@@ -0,0 +1,15 @@
+package ipheader
+
+/*IP首部*/
+type IpHeader struct {
+	VerHlen					uint8		/*IP首部的版本和首部长度, 各占4位*/
+	Service					uint8		/*服务类型*/
+	Length 					uint16		/*数据报总长度（数据长度+hlen字段*4）*/
+	Identification			uint16		/*标识*/
+	FlagsAndFragementOffset	uint16		/*前3位为标志，后13位为分片偏移*/
+	TimeToLive				uint8		/*生存时间*/
+	Protocol				uint8		/*协议, 定义了上层协议*/
+	HeaderChecksum			uint16		/*首部校验和*/
+	SourceAddress			uint32		/*源IP地址*/	
+	DestinationAddress		uint32		/*目的IP地址*/	
+}
