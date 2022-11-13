7840b76f8cc01565b8d883786f91f70d1182907b
basi <basi-a@outlook.com>
2022-11-13 14:00:39 +0800

    .

--- softwarePkg/ipModules.go
+++ softwarePkg/ipModules.go
@@ -8,6 +8,29 @@ import (
 )
 
 /*
+IPv4首部校验和计算
+*/
+func IPv4CheckSum(data []byte) uint16 {
+	var sum uint32
+	var length int = len(data)
+	var index int
+	/*以每16位为单位进行求和, 直到所有的字节全部求完或者只剩下一个8位字节(如果剩余一个8位字节说明字节数为奇数个)*/
+	for length > 1 {
+		sum += uint32(data[index])<<8 + uint32(data[index+1])
+		index += 2
+		length -= 2
+	}
+	/*如果字节数为奇数个, 要加上最后剩下的那个8位字节*/
+	if length > 0 {
+		sum += uint32(data[index])
+	}
+	/*加上高16位进位的部分*/
+	sum += (sum >> 16)
+	/*返回的时候求反*/
+	return uint16(^sum)
+}
+
+/*
 首部添加模块
 */
 func IpAddingModule(src_addr uint32, dest_addr uint32) (*iphdr.IpHeader, []uint8) {
@@ -29,7 +52,7 @@ func IpAddingModule(src_addr uint32, dest_addr uint32) (*iphdr.IpHeader, []uint8
 }
 
 /*
-处理模块, 当目的地址与本地地址相匹配时, 将数据报发送到重装模块, 否则发送到转发模块 
+处理模块, 当目的地址与本地地址相匹配时, 将数据报发送到重装模块, 否则发送到转发模块
 */
 func IpProcessingModule(header *iphdr.IpHeader, data []uint8, native_addr uint32, gateway_addr uint32)  {
 	/*目的地址与本地地址相匹配时, 将数据报发送到重装模块*/
@@ -63,27 +86,3 @@ func IpReassemblyModule(header *iphdr.IpHeader, data []uint8)  {
 func IpForwardingModule(header *iphdr.IpHeader, data []uint8){
 
 }
-
-/*
-IPv4首部校验和计算
-*/
-func IPv4CheckSum(data []byte) uint16 {
-	var sum uint32
-	var length int = len(data)
-	var index int
-	/*以每16位为单位进行求和, 直到所有的字节全部求完或者只剩下一个8位字节(如果剩余一个8位字节说明字节数为奇数个)*/
-	for length > 1 {
-		sum += uint32(data[index])<<8 + uint32(data[index+1])
-		index += 2
-		length -= 2
-	}
-	/*如果字节数为奇数个, 要加上最后剩下的那个8位字节*/
-	if length > 0 {
-		sum += uint32(data[index])
-	}
-	/*加上高16位进位的部分*/
-	sum += (sum >> 16)
-	/*返回的时候求反*/
-	return uint16(^sum)
-}
-
