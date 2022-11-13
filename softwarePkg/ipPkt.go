8e8cf482ae4cba868ed553a87079d9053b6d77f4
basi <basi-a@outlook.com>
2022-11-13 20:32:48 +0800

    Modify the code details

--- softwarePkg/ipPkt.go
+++ softwarePkg/ipPkt.go
@@ -15,7 +15,7 @@ func IpPkt(src_addr uint32, dest_addr uint32) (*iphdr.IpHeader, []uint8){
 	var IP_TTL 			uint8 = 0x80		/*TTL=128*/
 	var IP_PROTOCOL 	uint8 = 0x01		/*上层协议类型, 此处0x01表示ICMP*/
 	/*为IP首部各字段赋值*/
-	ipheader := iphdr.IpHeader{
+	header := iphdr.IpHeader{
 		VerHlen: IP_VER_HLEN,
 		Service: 0x00,
 		Length: 32,
@@ -27,12 +27,12 @@ func IpPkt(src_addr uint32, dest_addr uint32) (*iphdr.IpHeader, []uint8){
 		SourceAddress: src_addr,
 		DestinationAddress: dest_addr,
 	}
-	IP_HEADER_LEN := binary.Size(ipheader)	/*获取首部长度*/
-	IP_DATA_LEN := ipheader.Length-uint16(IP_HEADER_LEN)//32-20=12
+	IP_HEADER_LEN := binary.Size(header)	/*获取首部长度*/
+	IP_DATA_LEN := header.Length-uint16(IP_HEADER_LEN)//32-20=12
 	var data = make([]uint8, IP_DATA_LEN)	
 	//hex(yyz) : 0x79797a
 	data[0] = 0x79
 	data[1] = 0x79
 	data[2] = 0x7a
-	return &ipheader, data
+	return &header, data
 }
