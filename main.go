eb4cdaa4d702af974296cfa6b2dcd3f2920b227a
basi <basi-a@outlook.com>
2022-11-14 16:37:29 +0800

    update

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main()  {
-	log.Println("+++++++++++++++IP协议模拟程序  开始模拟+++++++++++++++")
+	log.Println("+++++++++++++++IP协议模拟程序-开始模拟+++++++++++++++")
 	fmt.Println()
 	log.Println("发送端发送数据报......")
 	header, data := pc.Send()
